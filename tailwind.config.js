343910669ad145c63576254759adb712f18c6724
Nouman Masood <noumanmasood967@gmail.com>
2025-10-19 11:21:53 +0500

    Finalizing Tailwind installation and configuration in root directory.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+ content: [
+    // This tells Tailwind to scan all HTML and JS files inside the 'frontend' folder
+    "./frontend/**/*.{html,js}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
